9d15a04dc004313263b5d0b6830734aa2a6697ba
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-15 21:48:51 +0100

    particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,10 +8,26 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float levelReloadDelaySeconds = 1;
+    [SerializeField] ParticleSystem deathEffect; //player explosion
+    [SerializeField] GameObject head; //player head
+    [SerializeField] GameObject legs;//player legs
+    private Rigidbody2D rigidBody; //the physics of the player
+    private bool hitGroundYet; //needed to prevent double particle effect
+
+    //needed start to have a non static value for rigidBody
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();//at game start get the physics
+        hitGroundYet = false; //needed to prevent double particle effect
+    }
 
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.tag == "Ground") {
+        if (other.tag == "Ground" && !hitGroundYet) {
+            hitGroundYet = true;
+            rigidBody.velocity = Vector3.zero;
             Debug.Log("Hit ground");
+            deathEffect.Play();
+            Destroy(head);
+            Destroy(legs);
             Invoke("ReloadScene", levelReloadDelaySeconds);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float levelReloadDelaySeconds = 1; //Delay for level reload
+    [SerializeField] ParticleSystem finishLineEffect;
 
     //Triggered on reaching finish line
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
             Debug.Log("You Finished!");
+            finishLineEffect.Play();
             Invoke("ReloadScene", levelReloadDelaySeconds);
         }
     }
