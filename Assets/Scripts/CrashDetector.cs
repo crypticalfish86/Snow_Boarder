6e8548f36a1c3b5456157827b4aa1ea33d50e900
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-17 16:56:27 +0100

    added snowtrail particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -12,6 +12,7 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] ParticleSystem deathEffect; //player explosion
     [SerializeField] GameObject head; //player head
     [SerializeField] GameObject legs;//player legs
+    [SerializeField] GameObject snowTrail; //player snowtrail
     [SerializeField] GameObject Camera; //the camera for the game
     
     private CinemachineBrain cinemachineBrain; //the cinemachine brain for the camera
@@ -26,20 +27,25 @@ public class CrashDetector : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other){
-        if (other.tag == "Ground" && !hitGroundYet) {
+        if (other.gameObject.CompareTag("Ground") && !hitGroundYet) {
             hitGroundYet = true; //prevent double play
             rigidBody.velocity = Vector3.zero; //stop player object movement
             Debug.Log("Hit ground");// console log
             deathEffect.Play(); //particle explosion
-            Destroy(head); //delete player
-            Destroy(legs); //delete player
+            deletePlayer();
             Destroy(cinemachineBrain); //stop camera
-            Invoke("ReloadScene", levelReloadDelaySeconds); //reload game
+            Invoke("reloadScene", levelReloadDelaySeconds); //reload game
         }
     }
 
     //Restarts level if snowboarder hits head
-    private void ReloadScene(){
+    private void reloadScene(){
         SceneManager.LoadScene("Snow_Boarder");
+    }
+
+    private void deletePlayer(){
+            Destroy(head); //delete player head
+            Destroy(legs); //delete player bottom
+            Destroy(snowTrail);//delete snowboard particle effect
     }
 }
