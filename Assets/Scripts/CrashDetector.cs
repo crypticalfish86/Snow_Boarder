f352503d63213f272cf0a8af8073ac56f857bfc8
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-17 15:59:26 +0100

    fixed camera on death

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -11,24 +12,29 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] ParticleSystem deathEffect; //player explosion
     [SerializeField] GameObject head; //player head
     [SerializeField] GameObject legs;//player legs
+    [SerializeField] GameObject Camera; //the camera for the game
+    
+    private CinemachineBrain cinemachineBrain; //the cinemachine brain for the camera
     private Rigidbody2D rigidBody; //the physics of the player
     private bool hitGroundYet; //needed to prevent double particle effect
 
     //needed start to have a non static value for rigidBody
     private void Start() {
+        cinemachineBrain = Camera.GetComponent<CinemachineBrain>(); //get the logic system of cinemachine
         rigidBody = GetComponent<Rigidbody2D>();//at game start get the physics
         hitGroundYet = false; //needed to prevent double particle effect
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if (other.tag == "Ground" && !hitGroundYet) {
-            hitGroundYet = true;
-            rigidBody.velocity = Vector3.zero;
-            Debug.Log("Hit ground");
-            deathEffect.Play();
-            Destroy(head);
-            Destroy(legs);
-            Invoke("ReloadScene", levelReloadDelaySeconds);
+            hitGroundYet = true; //prevent double play
+            rigidBody.velocity = Vector3.zero; //stop player object movement
+            Debug.Log("Hit ground");// console log
+            deathEffect.Play(); //particle explosion
+            Destroy(head); //delete player
+            Destroy(legs); //delete player
+            Destroy(cinemachineBrain); //stop camera
+            Invoke("ReloadScene", levelReloadDelaySeconds); //reload game
         }
     }
 
