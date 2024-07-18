03232d65bb4fb6aebf21ca644410ddb1377dd24e
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-18 22:18:32 +0100

    added sound effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -14,6 +14,7 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] GameObject legs;//player legs
     [SerializeField] GameObject snowTrail; //player snowtrail
     [SerializeField] GameObject Camera; //the camera for the game
+    [SerializeField] AudioClip crashSFX; //The SFX of the player crashing
     
     private CinemachineBrain cinemachineBrain; //the cinemachine brain for the camera
     private Rigidbody2D rigidBody; //the physics of the player
@@ -26,26 +27,44 @@ public class CrashDetector : MonoBehaviour
         hitGroundYet = false; //needed to prevent double particle effect
     }
 
+    //Triggers when player head touches level ground
     private void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.CompareTag("Ground") && !hitGroundYet) {
             hitGroundYet = true; //prevent double play
-            rigidBody.velocity = Vector3.zero; //stop player object movement
-            Debug.Log("Hit ground");// console log
-            deathEffect.Play(); //particle explosion
-            deletePlayer();
-            Destroy(cinemachineBrain); //stop camera
-            Invoke("reloadScene", levelReloadDelaySeconds); //reload game
+            PlayCrashEffects();
+            DeletePlayer();
+            Invoke("ReloadScene", levelReloadDelaySeconds); //reload game
         }
     }
-
-    //Restarts level if snowboarder hits head
-    private void reloadScene(){
-        SceneManager.LoadScene("Snow_Boarder");
+    
+    /*
+        Plays the effects on player crash:
+            1.Plays crash SFX
+            2.sets player velocity to zero
+            3.Stops camera motion
+            4.Triggers snow crash particle effect
+    */
+    private void PlayCrashEffects(){
+            rigidBody.velocity = Vector3.zero; //stop player object movement
+            rigidBody.Sleep(); //puts rigid body to sleep to prevent object start moving again
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Destroy(cinemachineBrain); //stop camera
+            deathEffect.Play(); //particle explosion
     }
 
-    private void deletePlayer(){
+    //Deletes player
+    private void DeletePlayer(){
             Destroy(head); //delete player head
             Destroy(legs); //delete player bottom
             Destroy(snowTrail);//delete snowboard particle effect
     }
+
+
+
+    //Restarts level if snowboarder hits head
+    private void ReloadScene(){
+        SceneManager.LoadScene("Snow_Boarder");
+    }
+
+
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,15 +6,27 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float levelReloadDelaySeconds = 1; //Delay for level reload
-    [SerializeField] ParticleSystem finishLineEffect;
+    [SerializeField] ParticleSystem finishLineEffect; //finish line confetti
+
+    private AudioSource levelFinishSoundEffect; //finish line sound effet
 
     //Triggered on reaching finish line
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
-            Debug.Log("You Finished!");
-            finishLineEffect.Play();
+            triggerLevelFinishEffects();
             Invoke("ReloadScene", levelReloadDelaySeconds);
         }
+    }
+
+    /*
+        Effects on level finish:
+        1.Activate confetti particle effect
+        2.Play the finish line sound effect
+    **/
+    private void triggerLevelFinishEffects(){
+            finishLineEffect.Play();
+            levelFinishSoundEffect = GetComponent<AudioSource>();
+            levelFinishSoundEffect.Play();
     }
 
     //Reloads level
