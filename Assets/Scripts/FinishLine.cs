957da515f206832bf3b922a861da1f6c7e17ef44
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-07-19 01:54:23 +0100

    gravity changes, deathboxes, basic level design

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -8,11 +8,14 @@ public class FinishLine : MonoBehaviour
     [SerializeField] float levelReloadDelaySeconds = 1; //Delay for level reload
     [SerializeField] ParticleSystem finishLineEffect; //finish line confetti
 
-    private AudioSource levelFinishSoundEffect; //finish line sound effet
+    private AudioSource levelFinishSoundEffect; //finish line sound effect
+
+    private bool hasFinished = false; //bool to prevent double trigger play
 
     //Triggered on reaching finish line
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Player"){
+        if(other.tag == "Player" && !hasFinished){
+            hasFinished = true;
             triggerLevelFinishEffects();
             Invoke("ReloadScene", levelReloadDelaySeconds);
         }
